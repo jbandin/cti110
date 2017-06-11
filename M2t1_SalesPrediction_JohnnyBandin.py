125f8f6d44dab5cda1f573f5f9497455afea5246
jbandin <bandinj9118@student.faytechcc.edu>
2017-06-11 15:19:11 -0400

    Add files via upload
    First submission M2T1_file

---
+++ M2t1_SalesPrediction_JohnnyBandin.py
@@ -0,0 +1,12 @@
+# Sales Preditcion Project
+# 11 JUN 2017
+# CTI-110 M2T1 - Sales Prediction
+# Johnny Bandin
+# Get the projected total sales
+total_sales = float(input('enter the projected sales: '))
+
+# Calculate the profit a 23 percent of total sales
+profit = total_sales * 0.23
+
+# Display the profit
+print('profit is $', format(profit, ',.2f')) 
